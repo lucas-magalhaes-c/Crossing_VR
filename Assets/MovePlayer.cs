141a340d64c7ce89d928f372a1fc6d3bdbd9daf5
Thales César <thalesgiriboni@gmail.com>
2020-09-28 16:12:15 -0300

    Add footsteps sound effect

--- Assets/MovePlayer.cs
+++ Assets/MovePlayer.cs
@@ -6,10 +6,12 @@ public class MovePlayer : MonoBehaviour
 {
     private bool shouldMove = false;
     public float movementSpeed = 2;
+    public AudioSource footstepsAudio;
     
     // Start is called before the first frame update
     void Start() {
         shouldMove = false;
+        footstepsAudio = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -20,9 +22,11 @@ public class MovePlayer : MonoBehaviour
 
     public void startMovement() {
         shouldMove = true;
+        footstepsAudio.Play(0);
     }
 
     public void endMovement() {
         shouldMove = false;
+        footstepsAudio.Pause();
     }
 }
