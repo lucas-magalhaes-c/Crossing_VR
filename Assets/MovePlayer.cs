7bffa385568e32267baffb6016d88d97a5c7c7bd
Thales César <thalesgiriboni@gmail.com>
2020-09-28 15:31:21 -0300

    Add player movement

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    private bool shouldMove = false;
+    public float movementSpeed = 2;
+    
+    // Start is called before the first frame update
+    void Start() {
+        shouldMove = false;
+    }
+
+    void Update()
+    {
+        if (shouldMove)
+            transform.position -= Vector3.forward * movementSpeed * Time.deltaTime;
+    }
+
+    public void startMovement() {
+        shouldMove = true;
+    }
+
+    public void endMovement() {
+        shouldMove = false;
+    }
+}
