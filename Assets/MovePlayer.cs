0a68b499deeff3802bf289b5ced1b01297a0e454
Thales César <thalesgiriboni@gmail.com>
2020-09-28 16:19:35 -0300

    Change move button material

--- Assets/MovePlayer.cs
+++ Assets/MovePlayer.cs
@@ -7,11 +7,14 @@ public class MovePlayer : MonoBehaviour
     private bool shouldMove = false;
     public float movementSpeed = 2;
     public AudioSource footstepsAudio;
+    public GameObject moveButtonObj;
+    private Renderer moveButton;
     
     // Start is called before the first frame update
     void Start() {
         shouldMove = false;
         footstepsAudio = GetComponent<AudioSource>();
+        moveButton = moveButtonObj.GetComponent<Renderer>();
     }
 
     void Update()
@@ -23,10 +26,12 @@ public class MovePlayer : MonoBehaviour
     public void startMovement() {
         shouldMove = true;
         footstepsAudio.Play(0);
+        moveButton.material.color = Color.red;
     }
 
     public void endMovement() {
         shouldMove = false;
         footstepsAudio.Pause();
+        moveButton.material.color = Color.blue;
     }
 }
