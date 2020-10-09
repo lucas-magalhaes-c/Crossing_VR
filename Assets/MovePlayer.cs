472dde1a0fd3c8b66341e793b80d8fca1c7b080f
Thales César <thalesgiriboni@gmail.com>
2020-10-08 21:35:50 -0300

    Move player to any x and z direction

--- Assets/MovePlayer.cs
+++ Assets/MovePlayer.cs
@@ -7,15 +7,18 @@ public class MovePlayer : MonoBehaviour
     private bool shouldMove = false;
     private bool finished = false;
     public float movementSpeed = 2;
+    public float moveButtonHeightDistance = 0.8f;
     public AudioSource footstepsAudio;
     public GameObject moveButtonObj;
     public GameObject finishWallObj;
+    private Camera mainCamera;
     private Renderer moveButton;
     private Renderer finishWall;
     
     // Start is called before the first frame update
     void Start() {
         shouldMove = false;
+        mainCamera = Camera.main;
         footstepsAudio = GetComponent<AudioSource>();
         moveButton = moveButtonObj.GetComponent<Renderer>();
         finishWall = finishWallObj.GetComponent<Renderer>();
@@ -23,8 +26,15 @@ public class MovePlayer : MonoBehaviour
 
     void Update()
     {
-        if (shouldMove)
-            transform.position -= Vector3.forward * movementSpeed * Time.deltaTime;
+        Vector3 newPosition;
+        moveButton.transform.position = new Vector3(moveButton.transform.position.x, mainCamera.transform.position.y + moveButtonHeightDistance, moveButton.transform.position.z);       
+        if (shouldMove) {
+            newPosition = mainCamera.transform.TransformDirection(new Vector3(
+                0,
+                0,
+                movementSpeed * Time.deltaTime));
+            transform.position += new Vector3(newPosition.x, 0, newPosition.z);
+        }
         if (!finished && transform.position.z < finishWallObj.transform.position.z + 5) {
             finished = true;
             finishWall.material.color = Color.green;
